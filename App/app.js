d9355977e83a8cdb881cbf6ee1269d4a63b7a971
yousufislamme <online.okk@gmail.com>
2023-04-18 11:00:36 +0600

    finish

--- App/app.js
+++ App/app.js
@@ -1,4 +1,4 @@
-let count = 10;
+let count = 0;
 let reset = 0;
 
 // set icon 
@@ -7,9 +7,10 @@ const setIconIncrease = `<ion-icon name="add-outline"></ion-icon>`;
 
 // decrease
 document.getElementById("decrease").onclick = function () {
-   count += 1;
+   count--;
 
    document.getElementById("count").innerHTML = count;
+   console.log('count:', count);
 
    // condition jodi decrease kre tahle text change hbe...
    if (count <= 3) {
@@ -17,20 +18,24 @@ document.getElementById("decrease").onclick = function () {
    } if (count == 0) {
       document.getElementById("increase").innerHTML = "Increase" + setIconIncrease;
    }
+
+
 }
 
 // Reset
 document.getElementById("reset").onclick = function () {
-   reset = 10;
+   count == reset;
    document.getElementById("count").innerHTML = reset;
    document.getElementById("increase").innerHTML = "Increase" + setIconIncrease;
+   console.log('reset:', reset);
 
 }
 // increase
 document.getElementById("increase").onclick = function () {
-   count -= 1;
+   count++;
    document.getElementById("count").innerHTML = count;
    // condition set
+   console.log('increase:', count);
 
    if (count >= 3) {
       document.getElementById("increase").innerHTML = "SubahanAllah" + setIconIncrease;
